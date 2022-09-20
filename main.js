b499b804ccc8c17f2a9a5d3149e9035f97f55bd7
Gabi Zanini <psicologia.plus@gmail.com>
2022-09-20 03:15:40 -0300

    Changed to async/await code design

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-const img = document.querySelector('img');
-const button = document.getElementById('fetch');
-const toSearch = document.getElementById('gif-search');
+const img = document.querySelector("img");
+const button = document.getElementById("fetch");
+const toSearch = document.getElementById("gif-search");
 
-window.addEventListener('load', () => {
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=M1PYUL5WucKjkUlOw4xGLaRAVjcF793Q&s=cats', {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url;
-  });
-})
+window.addEventListener("load", () => {
+ getImg("cats");
+});
 
-button.addEventListener('click', (e) => {
+button.addEventListener("click", (e) => {
   e.preventDefault();
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=M1PYUL5WucKjkUlOw4xGLaRAVjcF793Q&s=${toSearch.value}`, {mode: 'cors'})
-  .then(function(response) {
-    console.log(response.json);
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url;
-  });
-})
+  getImg(toSearch.value);
   
+});
+
+async function getImg(keyword) {
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=M1PYUL5WucKjkUlOw4xGLaRAVjcF793Q&s=${keyword}`,
+    { mode: "cors" }
+  );
+
+  response.json().then(function(response) {
+    img.src = response.data.images.original.url;
+  })
+  
+}
